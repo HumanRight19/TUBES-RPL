34b5a793226ff7a9ca9123ec76d0a1cd6a4bdb58
ALDO ATTALLAH DUTA HARDEANDRA <84911024+HumanRight19@users.noreply.github.com>
2022-06-27 10:44:56 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,19 @@ Route::get('/destinasi', [PlesirController::class, 'destinasiPage']);
 Route::get('/about', [PlesirController::class, 'aboutPage']);
 
 Route::get('/keraton', [PlesirController::class, 'keratonPage']);
+
+Route::get('/pura', [PlesirController::class, 'puraPage']);
+
+Route::get('/heritage', [PlesirController::class, 'heritagePage']);
+
+Route::get('/balekambang', [PlesirController::class, 'balekambangPage']);
+
+Route::get('/detailbayar', [PlesirController::class, 'detailbayarPage']);
+
+Route::get('/bukti', [PlesirController::class, 'buktiPage']);
+
+Route::get('/profile', [PlesirController::class, 'profilePage']);
+
+Route::get('/verify', [PlesirController::class, 'verifyPage']);
+
+Route::get('/forgot', [PlesirController::class, 'forgotPage']);
